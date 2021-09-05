ff11e349e0bc1487b0494adea10bffef8d12ef62
Thomas Overgaard <Thomasovergaard@hotmail.com>
2021-09-05 12:23:35 +0200

    added itemtype entitiy

---
+++ src/main/java/entitys/ItemType.java
@@ -0,0 +1,56 @@
+package entitys;
+
+import javax.persistence.*;
+
+@Table(name = "item_type")
+@Entity
+public class ItemType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    private String name;
+    private String description;
+    private int price;
+
+    public ItemType() {
+    }
+
+    public ItemType(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
