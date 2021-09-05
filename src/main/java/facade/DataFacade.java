2c574444ebcf4d1cf27f0353a1091fcd3fd24f3e
Thomas Overgaard <Thomasovergaard@hotmail.com>
2021-09-05 11:04:37 +0200

    all facade methods written out

---
+++ src/main/java/facade/DataFacade.java
@@ -0,0 +1,46 @@
+package facade;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class DataFacade {
+
+
+    public void createCustomer() {
+
+    }
+
+    public Customer getCustomer() {
+        return null;
+    }
+
+    public Customer getAllCustomers() {
+        return null;
+    }
+
+    public void createItemType() {
+
+
+    }
+
+    public ItemType findItemType() {
+        return null;
+    }
+
+    public void createOrderForCustomer() {
+
+    }
+
+    public OrderLine createOrderlineForOrder() {
+        return null;
+    }
+
+    public Order getAllOrders() {
+        return null;
+    }
+
+    public int totalCostOfOrder() {
+        return null;
+    }
+
+
+}
