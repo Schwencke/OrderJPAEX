9c036b5610501ae5ef6c87e97af94c1e7f6e0d7c
Thomas Overgaard <Thomasovergaard@hotmail.com>
2021-09-05 11:12:51 +0200

    added Customer entity

---
+++ src/main/java/entitys/Customer.java
@@ -0,0 +1,47 @@
+package entitys;
+
+import javax.persistence.*;
+
+@Table(name = "customer")
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    private String name;
+    private String email;
+
+    public Customer() {
+    }
+
+    public Customer(Integer id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
