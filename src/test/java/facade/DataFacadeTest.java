e6ecb617a91617c3b31659722bebbd66d6959cd9
Thomas Overgaard <Thomasovergaard@hotmail.com>
2021-09-05 12:01:16 +0200

    testing passed

--- src/test/java/facade/DataFacadeTest.java
+++ src/test/java/facade/DataFacadeTest.java
@@ -1,12 +1,30 @@
 package facade;
 
+import entitys.Customer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataFacadeTest {
+    DataFacade df;
+
+    @BeforeEach
+    void setUp() {
+        df = new DataFacade();
+        Customer q1 = new Customer("Thomas", "Thomasovergaard@hotmail.com");
+        javax.persistence.EntityManager em = facade.EntityManager.getEntityManager();
+        em.getTransaction().begin();
+        em.persist(q1);
+        em.getTransaction().commit();
+    }
 
     @Test
     public void getCustomer() {
+        int customerId = 1;
+        String actual = "";
+        String expected = "Thomas";
+        actual = df.getCustomer(customerId).getName();
+        assertEquals(actual, expected);
     }
 }
